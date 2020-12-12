1fd8c618771fb6f24fba4b80c4a7563d6af9b70f
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2020-12-12 22:25:14 +0100

    añadida clase usuario

---
+++ ProgramaGestor/src/dominio/Usuario.java
@@ -0,0 +1,26 @@
+package dominio;
+
+public class Usuario {
+	
+	private String nombre;
+	private String pass;
+	
+	public Usuario(String nombre, String pass) {
+		this.nombre = nombre;
+		this.pass = pass;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+}
